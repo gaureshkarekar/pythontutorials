a24148540779b3a8d731807052f572ffd39ad987
Gauresh Karekar <gaureshkarekar2326@gmail.com>
2024-06-07 09:50:25 +0530

    added files

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+name = input("Enter a name : ")
+namerev = name[::-1]
+
+if(name == namerev):
+    print(f"{name} is a palindrome")
+else:
+    print(f"{name} is not a palindrome")
+
+d = {"name":"Gauresh"}
---
+++ slicing.py
@@ -0,0 +1,13 @@
+arr = [5,6,7,8,9,0]
+    #  0 1 2 3 4 5 
+
+s = "Hello everyone my name is Gauresh"
+
+newString = s.split(" ")
+
+print(newString)
+
+print(newString[::-1])
+
+# arr reverse using for loop
+# implement split function using for and if else
