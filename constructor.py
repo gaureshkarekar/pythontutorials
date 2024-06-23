90ad4bfb060eb48d5c6ab79d3003ac5db26018c0
Gauresh Karekar <gaureshkarekar2326@gmail.com>
2024-06-23 12:25:09 +0530

    OOPS

---
+++ constructor.py
@@ -0,0 +1,13 @@
+
+class A:
+    name = "Gauresh"
+    def greet(self):
+        print("Hello My name is ",self.name)
+    
+    def printName(self):
+        print(self.name)
+
+obj = A()
+obj.greet()
+obj.printName()
+
